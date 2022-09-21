7174f805876a4c28ecb865e85b2f4a60fcae1194
hkjinnin <61165258+hkjinnin@users.noreply.github.com>
2022-09-21 13:59:06 +0900

    changed design,add url button

---
+++ background.js
@@ -0,0 +1,31 @@
+const now_date = () => {
+    let now = new Date();
+    n_d = now.getFullYear() + "/" + (now.getMonth() + 1) + "/" + now.getDate() + " : " + now.getHours() + "時" + now.getMinutes() + "分" + now.getSeconds() + "秒"
+    return n_d
+}
+const active_contextmenu = async () => {
+    await chrome.contextMenus.removeAll();
+    chrome.contextMenus.create({
+        type: 'normal',
+        id: "memo",
+        title: 'メモ',
+        contexts: ['selection']
+    });
+};
+
+chrome.runtime.onInstalled.addListener(active_contextmenu);
+chrome.runtime.onStartup.addListener(active_contextmenu);
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    if (info.menuItemId == "memo") {
+        now = now_date();
+        text = info.selectionText
+        url_link = tab.url;
+        title = tab.title;
+        content = [now, text, url_link, title];
+        chrome.storage.local.get(function (result) {
+            result["content"] = result["content"] || [];
+            result["content"].push(content);
+            chrome.storage.local.set(result);
+        });
+    };
+});
---
+++ js/app.js
@@ -0,0 +1,47 @@
+const $text_area = document.getElementsByTagName("textarea");
+const $delete_btn = document.getElementsByClassName("round-btn-delete")
+const $url_btn = document.getElementsByClassName("round-btn-url")
+const $first = document.getElementById("first");
+
+chrome.storage.local.get(['content'], function (result) {
+    try {
+        if (result.content.length == 0) {
+            throw 'No Element';
+        };
+        for (let i = result.content.length - 1; i >= 0; i--) {
+            $first.insertAdjacentHTML('beforeend', '<div class="text-area-div"><textarea style="height:' + ((result.content[i][1].length * 0.6) + 60) + 'px;">' + result.content[i][1] + '</textarea><span class="r-date">' + result.content[i][0] + '</span><a title="' + result.content[i][3] + '" class="round-btn-url"><i></i></a><a title="ダブルクリックで削除" class="round-btn-delete"></a></div>');
+        };
+        Array.from($text_area, e => {
+            e.addEventListener("input", e => {
+                c_change(Array.prototype.indexOf.call($text_area, e.target));
+            });
+        });
+        Array.from($delete_btn, e => {
+            e.addEventListener("dblclick", e => {
+                c_delete(Array.prototype.indexOf.call($delete_btn, e.target));
+            });
+        });
+        Array.from($url_btn, e => {
+            e.addEventListener("click", e => {
+                c_url(Array.prototype.indexOf.call($url_btn, e.target));
+            });
+        });
+    } catch (e) {
+        $first.insertAdjacentHTML('beforeend', '<div style="text-align: center;"><h2>右クリックしてメモ</h2></div>');
+    };
+    function c_change(n) {
+        result.content[(result.content.length - 1) - n][1] = $text_area[n].value;
+        chrome.storage.local.set(result);
+    }
+    function c_delete(n) {
+        result.content.splice((result.content.length - 1) - n, 1);
+        chrome.storage.local.set(result);
+        location.reload();
+    }
+    function c_url(n) {
+        window.open(result.content[(result.content.length - 1) - n][2])
+    }
+
+});
+
+
