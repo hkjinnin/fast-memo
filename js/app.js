2221de436b93d2ab35616c37f6d1d30df1687530
hkjinnin <61165258+hkjinnin@users.noreply.github.com>
2022-10-23 12:13:00 +0900

    update

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const $text_area = document.getElementsByTagName("textarea");
+const $body = document.getElementsByTagName("body")[0];
 const $delete_btn = document.getElementsByClassName("round-btn-delete");
 const $url_btn = document.getElementsByClassName("round-btn-url");
-const $first = document.getElementById("first");
 const $ta_div = document.getElementsByClassName("ta-div");
 
 chrome.runtime.onMessage.addListener(function (request) {
@@ -10,11 +10,6 @@ chrome.runtime.onMessage.addListener(function (request) {
     }
 });
 chrome.storage.local.get(["content"], function (result) {
-    const Observer = new ResizeObserver((entries) => {
-        let n = [].slice.call($text_area).indexOf(entries[0].target);
-        $first.style.width = entries[0].target.clientWidth + "px";
-        console.log(entries[0].target.clientWidth);
-    });
     function e_add(n, req) {
         switch (req) {
             case "c":
@@ -29,11 +24,11 @@ chrome.storage.local.get(["content"], function (result) {
             case "u":
                 window.open(result.content[result.content.length - 1 - n][2]);
                 break;
-            
         }
     }
     function s_c(i, n) {
-        let str = '<div class="ta-div"><textarea>' + result.content[i][n] + '</textarea><span class="r-date">' + result.content[i][0] + '</span><a title="' + result.content[i][3] + '" class="round-btn-url"><i></i></a><a title="ダブルクリックで削除" class="round-btn-delete"></a></div>';
+        let textarea_resize = '"height:' + (result.content[i][n].length + 60) + 'px"';
+        let str = '<div class="ta-div"><textarea style=' + textarea_resize + '>' + result.content[i][n] + '</textarea><span class="r-date">' + result.content[i][0] + '</span><a title="' + result.content[i][3] + '" class="round-btn-url"><i></i></a><a title="ダブルクリックで削除" class="round-btn-delete"></a></div>';
         return str;
     }
     try {
@@ -42,11 +37,12 @@ chrome.storage.local.get(["content"], function (result) {
         }
         for (let i = result.content.length - 1; i >= 0; i--) {
             if (result.content[i][1] == null) {
-                $first.insertAdjacentHTML("beforeend", s_c(i, 3));
+                $body.insertAdjacentHTML("beforeend", s_c(i, 3));
             } else {
-                $first.insertAdjacentHTML("beforeend", s_c(i, 1));
+                $body.insertAdjacentHTML("beforeend", s_c(i, 1));
             }
         }
+        $body.insertAdjacentHTML("beforeend", '<div class="last-div"></div>');
         Array.from($text_area, (e) => {
             e.addEventListener("input", (e) => {
                 e_add(Array.prototype.indexOf.call($text_area, e.target), "c");
@@ -62,10 +58,7 @@ chrome.storage.local.get(["content"], function (result) {
                 e_add(Array.prototype.indexOf.call($url_btn, e.target), "u");
             });
         });
-        Array.prototype.forEach.call($text_area, function (ta) {
-            Observer.observe(ta);
-        });
     } catch (e) {
-        $first.insertAdjacentHTML("beforeend", '<div style="text-align: center;"><h1>まだ何もメモしていません<br>右クリックメニューでメモできます</h1></div>');
+        $body.insertAdjacentHTML("beforeend", '<div style="text-align: center;"><h1>まだ何もメモしていません<br>右クリックメニューでメモできます</h1></div>');
     }
 });
