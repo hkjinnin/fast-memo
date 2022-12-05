0b78cc38c648b4dcffff334a6c0b49bfa6aad072
hkjinnin <61165258+hkjinnin@users.noreply.github.com>
2022-12-05 20:41:31 +0900

    Add files via upload

--- background.js
+++ background.js
@@ -1,8 +1,16 @@
 const now_date = () => {
     let now = new Date();
-    n_d = now.getFullYear() + "/" + (now.getMonth() + 1) + "/" + now.getDate() + " : " + now.getHours() + "時" + now.getMinutes() + "分" + now.getSeconds() + "秒";
-    return n_d;
+    let options = {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+    };
+    return new Intl.DateTimeFormat("ja-JP", options).format(now);
 };
+
 const active_contextmenu = async () => {
     await chrome.contextMenus.removeAll();
     chrome.contextMenus.create({
@@ -16,24 +24,25 @@ const active_contextmenu = async () => {
 chrome.runtime.onInstalled.addListener(active_contextmenu);
 chrome.runtime.onStartup.addListener(active_contextmenu);
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-    if (info.menuItemId == "memo") {
-        now = now_date();
-        text = info.selectionText;
-        url_link = tab.url;
-        title = tab.title;
-        content = [now, text, url_link, title];
-        chrome.storage.local.get(function (result) {
-            result["content"] = result["content"] || [];
-            result["content"].push(content);
-            chrome.storage.local.set(result);
-            try {
-                chrome.runtime.sendMessage({
-                    trigger: "memo",
-                });
-            } catch (error) {
-                console.log("not find reciver");
-            }
-            
-        });
+    switch (info.menuItemId) {
+        case "memo":
+            now = now_date();
+            text = info.selectionText;
+            url_link = tab.url;
+            title = tab.title;
+            content = [now, text, url_link, title];
+            chrome.storage.local.get(function (result) {
+                result["content"] = result["content"] || [];
+                result["content"].push(content);
+                chrome.storage.local.set(result);
+                try {
+                    chrome.runtime.sendMessage({
+                        trigger: "memo",
+                    });
+                } catch (error) {
+                    console.log("not find reciver");
+                }
+            });
+            break;
     }
 });
