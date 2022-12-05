6710298152f6a4925b71b59d462a01606d1ca769
hkjinnin <61165258+hkjinnin@users.noreply.github.com>
2022-12-05 20:52:05 +0900

    Add files via upload

--- js/edit.js
+++ js/edit.js
@@ -1,31 +1,49 @@
 chrome.storage.local.get(["content"], function (result) {
     const t = new URL(window.location.href).searchParams.get("t");
-    const n = result.content.length - 1 - t;
-    let text = result.content[n][1];
-    let url_link = result.content[n][2];
-    let title = result.content[n][3];
-    const $body = document.getElementsByTagName("body")[0];
-    
-    function self_chk () {
-        let pattern_check = new RegExp(chrome.runtime.id, "g");
-        return pattern_check.test(url_link);
-    };
-    if (text === null && self_chk()) {
+    const index = result.content.length - 1 - t;
+    let text = result.content[index][1];
+    let urlLink = result.content[index][2];
+    let title = result.content[index][3];
+    const body = document.getElementsByTagName("body")[0];
+
+    function isSelf() {
+        let patternCheck = new RegExp(chrome.runtime.id, "g");
+        return patternCheck.test(urlLink);
+    }
+
+    if (text === null && isSelf()) {
         text = "";
-    } else if (text === null && !self_chk()) {
+    } else if (text === null && !isSelf()) {
         text = title;
     }
-    let str = '<div class="ta-div"><textarea class="text-area-style">' + text + '</textarea><span class="r-date">' + result.content[n][0] + '</span><a title="ダブルクリックで削除" class="round-btn-delete"></a></div>';
-    $body.insertAdjacentHTML("beforeend", str);
-    const $textarea = document.getElementsByTagName("textarea")[0];
-    const $button = document.getElementsByTagName("a")[0];
-    $textarea.addEventListener("input", () => {
-        result.content[n][1] = $textarea.value;
+
+    const taDiv = document.createElement("div");
+    taDiv.classList.add("ta-div");
+
+    const textarea = document.createElement("textarea");
+    textarea.classList.add("text-area-style");
+    textarea.value = text;
+    taDiv.appendChild(textarea);
+
+    const rDate = document.createElement("span");
+    rDate.classList.add("r-date");
+    rDate.textContent = result.content[index][0];
+    taDiv.appendChild(rDate);
+
+    const button = document.createElement("a");
+    button.classList.add("round-btn-delete");
+    button.title = "ダブルクリックで削除";
+    taDiv.appendChild(button);
+
+    body.appendChild(taDiv);
+
+    textarea.addEventListener("input", () => {
+        result.content[index][1] = textarea.value;
         chrome.storage.local.set(result);
     });
-    $button.addEventListener("dblclick", () => {
-        result.content[n][1] = $textarea.value;
-        result.content.splice(n, 1);
+    button.addEventListener("dblclick", () => {
+        result.content[index][1] = textarea.value;
+        result.content.splice(index, 1);
         chrome.storage.local.set(result);
         window.close(location.href);
     });
