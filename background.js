a710ddbb2dc173686b337628305247083da5c3c9
hkjinnin <hkjinnin@gmail.com>
2023-05-28 03:43:14 +0900

    update

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+// 現在の日時を取得しフォーマットする関数
 const now_date = () => {
     const now = new Date();
     const options = {
@@ -11,6 +12,7 @@ const now_date = () => {
     return new Intl.DateTimeFormat("ja-JP", options).format(now);
 };
 
+// コンテキストメニューを設定する関数
 const active_contextmenu = async () => {
     await chrome.contextMenus.removeAll();
     chrome.contextMenus.create({
@@ -21,21 +23,28 @@ const active_contextmenu = async () => {
     });
 };
 
+// 拡張機能がインストールまたは更新された際にコンテキストメニューを設定
 chrome.runtime.onInstalled.addListener(active_contextmenu);
+// ブラウザが起動した際にコンテキストメニューを設定
 chrome.runtime.onStartup.addListener(active_contextmenu);
+
+// コンテキストメニューがクリックされた時のイベントリスナー
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
     switch (info.menuItemId) {
+        // "メモ"がクリックされた場合
         case "memo":
             const now = now_date();
             const text = info.selectionText;
             const url_link = tab.url;
             const title = tab.title;
             const content = [now, text, url_link, title];
+            // メモをローカルストレージに保存
             chrome.storage.local.get(["content"], function (result) {
                 result.content = result.content || [];
                 result.content.push(content);
                 chrome.storage.local.set(result);
                 try {
+                    // メモが保存されたことを他のパーツに通知
                     chrome.runtime.sendMessage({
                         trigger: "memo",
                     });
--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
+// Memoクラスの定義
 class Memo {
+    // コンストラクタで初期化
     constructor(n, content) {
         this.n = n;
         this.date = content[n][0];
@@ -6,6 +8,8 @@ class Memo {
         this.url_link = content[n][2];
         this.title = this.escapeHtml(content[n][3]);
     }
+    
+    // HTMLエスケープ処理
     escapeHtml(target) {
         if (typeof target !== "string") {
             return target;
@@ -22,18 +26,21 @@ class Memo {
         });
     }
 
+    // url_linkが現在のランタイムIDを含むかチェック
     self_chk() {
         let pattern_check = new RegExp(chrome.runtime.id, "g");
         return pattern_check.test(this.url_link);
     }
 
+    // メモのタイプに応じて編集ボタンを切り替える
     switch_btn() {
         if (this.type_chk() === "memo") {
-            return '<a style="right: 55px" class="round-btn-edit"></a>';
+            return '<a title="memo画面で編集" style="right: 55px" class="round-btn-edit"></a>';
         }
         return '<a title="memo画面で編集" class="round-btn-edit"></a><a title="' + this.title + '" class="round-btn-url"></a>';
     }
 
+    // メモのタイプをチェック
     type_chk() {
         if (this.self_chk()) {
             return "memo";
@@ -44,6 +51,7 @@ class Memo {
         }
     }
 
+    // メモのタイプに応じて表示するテキストを切り替える
     switch_text() {
         switch (this.type_chk()) {
             case "memo":
@@ -58,16 +66,20 @@ class Memo {
         }
     }
 
+    // テキストエリアの高さを計算
     ta_height() {
         const font_size = 16;
         const height = (this.switch_text().length * (font_size / 15)) + 10;
         return height;
     }
 
+    // メモのコンテンツを生成
     content() {
         return '<div class="ta-div"><textarea class="textarea-style" style="height:' + this.ta_height() + 'px">' + this.switch_text() + '</textarea><span class="r-date">' + this.date + '</span>' + this.switch_btn() + '<a title="ダブルクリックで削除" class="round-btn-delete"></a></div>';
     }
 }
+
+// メモの追加、更新、削除、URLの開き方を制御
 function e_add(n, req, result) {
     switch (req) {
         case "s":
@@ -89,6 +101,8 @@ function e_add(n, req, result) {
             break;
     }
 }
+
+// 与えられたクラス名を持つすべての要素にイベントリスナーを追加する関数
 function addEventListenerByClass(className, event, handler) {
     const elements = document.getElementsByClassName(className);
     Array.from(elements, (e) => {
@@ -96,12 +110,14 @@ function addEventListenerByClass(className, event, handler) {
     });
 }
 
+// "memo"メッセージを受け取ったときにページをリロードする
 chrome.runtime.onMessage.addListener(function (request) {
     if (request.trigger == "memo") {
         location.reload();
     }
 });
 
+// ローカルストレージからデータを取得し、メモを表示
 chrome.storage.local.get(["content"], function (result) {
     const $main = document.getElementById("main");
     try {
@@ -113,11 +129,13 @@ chrome.storage.local.get(["content"], function (result) {
             $main.insertAdjacentHTML("beforeend", memo.content());
         }
 
+        // イベントハンドラーの設定
         const handleEvent = (e, elements, action) => {
             const index = Array.prototype.indexOf.call(elements, e.target);
             e_add(index, action, result);
         };
 
+        // 各クラスに対してイベントリスナーを追加
         addEventListenerByClass("textarea-style", "input", (e) => {
             handleEvent(e, document.getElementsByTagName("textarea"), "s", result);
         });
@@ -135,10 +153,6 @@ chrome.storage.local.get(["content"], function (result) {
         });
 
     } catch (e) {
-        if (e === "No Element") {
-            $main.insertAdjacentHTML("beforeend", '<div class="memo-div">使い方</div>');
-        } else {
-            console.log(e);
-        }
+        console.log(e);
     }
 });
